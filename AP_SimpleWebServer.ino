a3e4175ab35a748dc3835ede43250e4d27af501c
Markus <markus.hakala@kolumbus.fi>
2022-01-18 17:37:00 +0200

    commit first version

---
+++ AP_SimpleWebServer.ino
@@ -0,0 +1,502 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "Adafruit_TSL2591.h"
+#include "arduino_secrets.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+// connect SCL to I2C Clock
+// connect SDA to I2C Data
+// connect Vin to 3.3-5V DC
+// connect GROUND to common ground
+
+unsigned long delayTime;
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID; // your network SSID (name)
+char pass[] = SECRET_PASS; // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;
+
+// your network key index number (needed only for WEP)
+const int ledRed = 6;
+const int motionSensor = 3;
+boolean state = false;
+
+// Checks if motion was detected
+void detectsMovement()
+{
+  state = true;
+}
+
+int led = LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+  // PIR Motion Sensor mode INPUT_PULLUP
+  pinMode(motionSensor, INPUT);
+  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+  // Set LED to LOW
+  pinMode(ledRed, OUTPUT);
+  digitalWrite(ledRed, LOW);
+
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Access Point Web Server");
+
+  pinMode(led, OUTPUT); // set the LED pin mode
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // by default the local IP address will be 192.168.4.1
+  // you can override it with the following:
+  // WiFi.config(IPAddress(10, 0, 0, 1));
+
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+
+  // Create open network. Change this line if you want to create an WEP network:
+  status = WiFi.beginAP(ssid, pass);
+  if (status != WL_AP_LISTENING)
+  {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  // wait 10 seconds for connection:
+  delay(10000);
+
+  setupTSL2591();
+  setupBME280();
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
+}
+
+void loop()
+{
+  // compare the previous status to the current status
+  if (status != WiFi.status())
+  {
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if (status == WL_AP_CONNECTED)
+    {
+      // a device has connected to the AP
+      Serial.println("Device connected to AP");
+    }
+    else
+    {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
+
+  String motion = "Ei";
+  
+  if (state == true)
+  {
+    Serial.println("Liiketta havaittu");
+    motion = "KYLLÄ";
+    state = false;
+  }
+
+  digitalWrite(ledRed, digitalRead(motionSensor));
+
+  // read values from sensors
+  float temp = bme.readTemperature();
+  float humidity = bme.readHumidity();
+  float pressureMbar = bme.readPressure() * 0.01F; // convert to mBar
+
+  // read luminosity and calculate lux
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  float luminosity = tsl.calculateLux(full, ir);
+
+  // make strings to present values in the client
+  String printTemp = String(temp, 1) + " *C";
+  String printHumidity = String(humidity, 0) + " %";
+  String printPressure = String(pressureMbar, 0) + " mbar";
+  String printLuminocity = String(luminosity, 0) + " lux";
+
+  WiFiClient client = server.available(); // listen for incoming clients
+
+  if (client)
+  {                               // if you get a client,
+    Serial.println("new client"); // print a message out the serial port
+    String currentLine = "";      // make a String to hold incoming data from the client
+    while (client.connected())
+    {                        // loop while the client's connected
+      delayMicroseconds(10); // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
+      if (client.available())
+      {                         // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out the serial monitor
+        if (c == '\n')
+        { // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)
+          {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html; charset=utf-8");
+            client.println();
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" charset=\"UTF-8\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+            // CSS to style
+            client.println("<style>");
+            client.println("table, th, td { border: 0; text-align: center; vertical-align: middle;} .button { background-color: white; border: solid; color: black; padding: 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px;} .buttonUpdate {border-radius: 12px;}");
+            client.println("</style>");
+            // Web Page Heading
+            client.println("</head><body><h1>Mittaukset</h1>");
+            client.println("<table>");
+
+            client.println("<tr>");
+            client.println("<td>Lämpötila </td>");
+            client.println("<td>" + printTemp + "</td>");
+            client.println("</tr>");
+
+            client.println("<tr>");
+            client.println("<td>Kosteus </td>");
+            client.println("<td>" + printHumidity + "</td>");
+            client.println("</tr>");
+
+            client.println("<tr>");
+            client.println("<td>Paine </td>");
+            client.println("<td>" + printPressure + "</td>");
+            client.println("</tr>");
+
+            client.println("<tr>");
+            client.println("<td>Valoisuus </td>");
+            client.println("<td>" + printLuminocity + "</td>");
+            client.println("</tr>");
+
+            client.println("<tr>");
+            client.println("<td>Paikalla? </td>");
+            client.println("<td>" + motion + "</td>");
+            client.println("</tr>");
+            
+            client.println("<tr>");
+            client.println("<td colspan=\"2\"><button class=\"button buttonUpdate\" onClick=\"window.location.reload();\">Update</button></td>");
+            client.println("</tr>");
+            client.println("</table>");
+            
+            client.println("</body></html>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else
+          { // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {                   // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void printWiFiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.println("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
+
+void setupBME280()
+{
+  unsigned status;
+
+  // default settings
+  status = bme.begin(0x76);
+  // You can also pass in a Wire library object like &Wire2
+  // status = bme.begin(0x76, &Wire2)
+  if (!status)
+  {
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring, address, sensor ID!"));
+    Serial.print(F("SensorID was: 0x"));
+    Serial.println(bme.sensorID(), 16);
+    Serial.print(F("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n"));
+    Serial.print(F("   ID of 0x56-0x58 represents a BMP 280,\n"));
+    Serial.print(F("        ID of 0x60 represents a BME 280.\n"));
+    Serial.print(F("        ID of 0x61 represents a BME 680.\n"));
+    while (1)
+      delay(10);
+  }
+  else
+  {
+    Serial.print(F("Found a BME280 sensor"));
+  }
+}
+
+void printBME280Values()
+{
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
+
+  Serial.print("Pressure = ");
+
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
+}
+
+void printTSL2591Values(void)
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.print(F("Sensor:       "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:   "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:    "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:    "));
+  Serial.print(sensor.max_value);
+  Serial.println(F(" lux"));
+  Serial.print(F("Min Value:    "));
+  Serial.print(sensor.min_value);
+  Serial.println(F(" lux"));
+  Serial.print(F("Resolution:   "));
+  Serial.print(sensor.resolution, 4);
+  Serial.println(F(" lux"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2591
+*/
+/**************************************************************************/
+void configureSensor(void)
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED); // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+
+  /* Display the gain and integration time for reference sake */
+  Serial.println(F("------------------------------------"));
+  Serial.print(F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch (gain)
+  {
+  case TSL2591_GAIN_LOW:
+    Serial.println(F("1x (Low)"));
+    break;
+  case TSL2591_GAIN_MED:
+    Serial.println(F("25x (Medium)"));
+    break;
+  case TSL2591_GAIN_HIGH:
+    Serial.println(F("428x (High)"));
+    break;
+  case TSL2591_GAIN_MAX:
+    Serial.println(F("9876x (Max)"));
+    break;
+  }
+  Serial.print(F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC);
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+}
+
+/**************************************************************************/
+/*
+    Program entry point for the Arduino sketch
+*/
+/**************************************************************************/
+void setupTSL2591(void)
+{
+
+  if (tsl.begin())
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  }
+  else
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1)
+      ;
+  }
+
+  /* Display some basic information on this sensor */
+  printTSL2591Values();
+
+  /* Configure the sensor */
+  configureSensor();
+
+  // Now we're ready to get readings ... move on to loop()!
+}
+
+/**************************************************************************/
+/*
+    Shows how to perform a basic read on visible, full spectrum or
+    infrared light (returns raw 16-bit ADC values)
+*/
+/**************************************************************************/
+void simpleRead(void)
+{
+  // Simple data read example. Just read the infrared, fullspecrtrum diode
+  // or 'visible' (difference between the two) channels.
+  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
+  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
+
+  Serial.print(F("[ "));
+  Serial.print(millis());
+  Serial.print(F(" ms ] "));
+  Serial.print(F("Luminosity: "));
+  Serial.println(x, DEC);
+}
+
+/**************************************************************************/
+/*
+    Show how to read IR and Full Spectrum at once and convert to lux
+*/
+/**************************************************************************/
+void advancedRead(void)
+{
+  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // That way you can do whatever math and comparisons you want!
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  Serial.print(F("[ "));
+  Serial.print(millis());
+  Serial.print(F(" ms ] "));
+  Serial.print(F("IR: "));
+  Serial.print(ir);
+  Serial.print(F("  "));
+  Serial.print(F("Full: "));
+  Serial.print(full);
+  Serial.print(F("  "));
+  Serial.print(F("Visible: "));
+  Serial.print(full - ir);
+  Serial.print(F("  "));
+  Serial.print(F("Lux: "));
+  Serial.println(tsl.calculateLux(full, ir), 6);
+}
+
+/**************************************************************************/
+/*
+    Performs a read using the Adafruit Unified Sensor API.
+*/
+/**************************************************************************/
+void unifiedSensorAPIRead(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  tsl.getEvent(&event);
+
+  /* Display the results (light is measured in lux) */
+  Serial.print(F("[ "));
+  Serial.print(event.timestamp);
+  Serial.print(F(" ms ] "));
+  if ((event.light == 0) |
+      (event.light > 4294966000.0) |
+      (event.light < -4294966000.0))
+  {
+    /* If event.light = 0 lux the sensor is probably saturated */
+    /* and no reliable data could be generated! */
+    /* if event.light is +/- 4294967040 there was a float over/underflow */
+    Serial.println(F("Invalid data (adjust gain or timing)"));
+  }
+  else
+  {
+    Serial.print(event.light);
+    Serial.println(F(" lux"));
+  }
+}
